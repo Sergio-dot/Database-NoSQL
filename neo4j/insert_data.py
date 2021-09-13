565a44bd1917ded6085ba35830174a0fdcc3489c
Sergio-dot <sergio.siclari@gmail.com>
2021-09-13 14:38:20 +0200

    Commit to main branch

--- neo4j/insert_data.py
+++ neo4j/insert_data.py
@@ -1,101 +1,67 @@
 from neo4j import GraphDatabase
+import neo4j
+from neo4j.work import result
 
 uri = "bolt://localhost:7687"
 user = 'neo4j'
 psw = 'sdp'
 
 driver = GraphDatabase.driver(uri, auth=(user, psw))
-session = driver.session()
+pathCSV="file:///dataset.csv"
 
-##### CUSTOMER #####
-session.run("CREATE (c:CUSTOMER_NAME);")
-session.run("""LOAD CSV WITH HEADERS FROM "file:///dataset.csv" AS line
-FIELDTERMINATOR ','
-WITH line
-WHERE line.CUSTOMER_NAME IS NOT NULL
-MERGE (c: CUSTOMER_NAME {CUSTOMER_NAME: line.CUSTOMER_NAME})""")
+def CSVImportCustomer(self, pathCSV):
+    session = driver.session()
+    try:
+        session.run("CREATE (c:CUSTOMER_NAME);")
+    except:
+        print("Errore")
+    print("Importing customer...")
+    result = session.run("""USING PERIODIC COMMIT 2000
+    LOAD CSV WITH HEADERS FROM \"""" + str(pathCSV) + """\" AS line
+    FIELDTERMINATOR ','
+    MERGE (c:CUSTOMER_NAME {CUSTOMER_NAME: line.CUSTOMER_NAME});""")
+    print("added " + str(result.consume().counters.nodes_created) + " nodes.")
+    session.close()
 
-##### STORE #####
-session.run("CREATE (s:STORE_NAME);")
-session.run("""LOAD CSV WITH HEADERS FROM "file:///dataset.csv" AS line
-FIELDTERMINATOR ','
-WITH line
-WHERE line.STORE_NAME IS NOT NULL
-MERGE (s: STORE_NAME {STORE_NAME: line.STORE_NAME})""")
+def CSVImportStore(self, pathCSV):
+    session = driver.session()
+    try:
+        session.run("CREATE (s:STORE_NAME);")
+    except:
+        print("Errore")
+    print("Importing store...")
+    result = session.run("""USING PERIODIC COMMIT 2000
+    LOAD CSV WITH HEADERS FROM \"""" + str(pathCSV) + """\" AS line
+    FIELDTERMINATOR ','
+    MERGE (s:STORE_NAME {STORE_NAME: line.STORE_NAME});""")
+    print("added " + str(result.consume().counters.nodes_created) + " nodes.")
+    session.close()
 
-##### AMOUNT #####
-session.run("CREATE (a:AMOUNT);")
-session.run("""LOAD CSV WITH HEADERS FROM "file:///dataset.csv" AS line
-FIELDTERMINATOR ','
-WITH line
-WHERE line.AMOUNT IS NOT NULL
-MERGE (a:AMOUNT {AMOUNT: line.AMOUNT})""")
+def CSVImportTransaction(self, pathCSV):
+    session = driver.session()
+    try:
+        print("Importing transactions to store from customer...")
+        result = session.run("""USING PERIODIC COMMIT 1000
+        LOAD CSV WITH HEADERS FROM \""""+ str(pathCSV) + """\" AS line
+        FIELDTERMINATOR ','
+        MATCH (c:CUSTOMER_NAME {CUSTOMER_NAME: line.CUSTOMER_NAME})
+        MATCH (s:STORE_NAME {STORE_NAME: line.STORE_NAME})
+        CREATE (c)-[:HAS_BOUGHT {
+            TRANSACTION_ID: line.ID,
+            DATE: line.TRANSACTION_DATE,
+            AMOUNT: line.AMOUNT,
+            STATUS: line.STATUS
+        }]->(s);""")
+        print("added " + str(result.consume().counters.relationships_created) + " relations.")
+    except:
+        print('errore')
+    session.close()
 
-##### TRANSACTION DATE #####
-session.run("CREATE (t:TRANSACTION_DATE);")
-session.run("""LOAD CSV WITH HEADERS FROM "file:///dataset.csv" AS line
-FIELDTERMINATOR ','
-WITH line
-WHERE line.TRANSACTION_DATE IS NOT NULL
-MERGE (t: TRANSACTION_DATE {TRANSACTION_DATE: line.TRANSACTION_DATE})""")
 
-##### STATUS #####
-session.run("CREATE (s:STATUS);")
-session.run("""LOAD CSV WITH HEADERS FROM "file:///dataset.csv" AS line
-FIELDTERMINATOR ','
-WITH line
-WHERE line.STATUS IS NOT NULL
-MERGE (s: STATUS {STATUS: line.STATUS})""")
+def main():
+    CSVImportCustomer(driver, pathCSV)
+    CSVImportStore(driver, pathCSV)
+    CSVImportTransaction(driver, pathCSV)
 
-#######################
-###### RELAZIONI ######
-#######################
-
-##### CUSTOMER - STORE #####
-session.run("""LOAD CSV WITH HEADERS FROM "file:///dataset.csv" AS line
-FIELDTERMINATOR ','
-MATCH (c:CUSTOMER_NAME {CUSTOMER_NAME: line.CUSTOMER_NAME})
-MATCH (s:STORE_NAME {STORE_NAME: line.STORE_NAME})
-MERGE (c)-[r:BOUGHT_FROM]->(s);""")
-
-##### CUSTOMER - AMOUNT #####
-session.run("""LOAD CSV WITH HEADERS FROM "file:///dataset.csv" AS line
-FIELDTERMINATOR ','
-MATCH (c:CUSTOMER_NAME {CUSTOMER_NAME: line.CUSTOMER_NAME})
-MATCH (a:AMOUNT {AMOUNT: line.AMOUNT})
-MERGE (c)-[r:SPENT]->(a);""")
-
-##### STORE - AMOUNT #####
-session.run("""LOAD CSV WITH HEADERS FROM "file:///dataset.csv" AS line
-FIELDTERMINATOR ','
-MATCH (s:STORE_NAME {STORE_NAME: line.STORE_NAME})
-MATCH (a:AMOUNT {AMOUNT: line.AMOUNT})
-MERGE (s)-[r:HAS_GAINED]->(a);""")
-
-##### STORE - TRANSACTION_DATE #####
-session.run("""LOAD CSV WITH HEADERS FROM "file:///dataset.csv" AS line
-FIELDTERMINATOR ','
-MATCH (s:STORE_NAME {STORE_NAME: line.STORE_NAME})
-MATCH (t:TRANSACTION_DATE {TRANSACTION_DATE: line.TRANSACTION_DATE})
-MERGE (s)-[r:REGISTERED]->(t);""")
-
-##### CUSTOMER - TRANSACTION DATE #####
-session.run("""LOAD CSV WITH HEADERS FROM "file:///dataset.csv" AS line
-FIELDTERMINATOR ','
-MATCH (c:CUSTOMER_NAME {CUSTOMER_NAME: line.CUSTOMER_NAME})
-MATCH (t:TRANSACTION_DATE {TRANSACTION_DATE: line.TRANSACTION_DATE})
-MERGE (c)-[r:HAS_MADE]->(t);""")
-
-##### TRANSACTION_DATE - AMOUNT #####
-session.run("""LOAD CSV WITH HEADERS FROM "file:///dataset.csv" AS line
-FIELDTERMINATOR ','
-MATCH (t:TRANSACTION_DATE {TRANSACTION_DATE: line.TRANSACTION_DATE})
-MATCH (a:AMOUNT {AMOUNT: line.AMOUNT})
-MERGE (t)-[r:IMPORT]->(a);""")
-
-##### TRANSACTION_DATE - STATUS #####
-session.run("""LOAD CSV WITH HEADERS FROM "file:///dataset.csv" AS line
-FIELDTERMINATOR ','
-MATCH (t:TRANSACTION_DATE {TRANSACTION_DATE: line.TRANSACTION_DATE})
-MATCH (s:STATUS {STATUS: line.STATUS})
-MERGE (t)-[r:HAS_STATUS]->(s);""")
+if __name__ == "__main__":
+    main()

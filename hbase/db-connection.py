d394c4fb9d88b27fd4f481ba84f2406829c8ef31
Sergio-dot <sergio.siclari@gmail.com>
2021-09-13 19:40:21 +0200

    Working data insertion function

--- hbase/db-connection.py
+++ hbase/db-connection.py
@@ -2,12 +2,58 @@
 
 import csv
 import happybase
+from happybase import connection
 
 def connection_db():
-    conn = happybase.Connection('127.0.0.1', 9090)
-    conn.open()
-    return conn
+    try:
+        conn = happybase.Connection()
+        conn.open()
+        return conn
+    except Exception as e:
+        print("Error: " + e)
 
+def create_table():
+    try:
+        connection = connection_db()
+        connection.create_table('CUSTOMER', {'cf1':dict(max_versions=1) })
+        print("Table created")
+    except Exception as e:
+        print("Error " + e)
+        connection.close()
+
+def push_data():
+    try:
+        connection = connection_db()
+        table = connection.table('CUSTOMER')
+        csvfile = open("dataset.csv", "r")
+        csvreader = csv.reader(csvfile)
+        #input = csv.DictReader(open('dataset.csv'))
+        for row in csvreader:
+            table.put(row[0], {'cf1:CUST_NAME':row[1] })
+    except Exception as e:
+        print("Error " + e)
+        connection.close()
+
+def scan_table():
+    try:
+        connection = connection_db()
+        table = connection.table('CUSTOMER')
+        for key, data in table.scan():
+            no = key
+            for value in data.items():
+                cf1 = value
+                print(no, cf1)
+    except Exception as e:
+        print("Error " + e)
+
+connection_db()
+#create_table()
+#push_data()
+scan_table()
+
+
+
+"""
 def insert_customer(conn, customer, row_key):
     table = conn.table('customer')
     table.put(str(row_key), 'customer')
@@ -26,3 +72,4 @@ with open('dataset.csv') as csv_file:
             line_count +=1
     print(f'Processed {line_count} lines.')
     
+"""
